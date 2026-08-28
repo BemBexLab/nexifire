f957511453949bcda91baa080f8f48bdcd1cd99f
car5p3 <triggred47@icloud.com>
2026-08-29 03:52:45 +0500

    Spelling update

--- components/HowWeWork.tsx
+++ components/HowWeWork.tsx
@@ -32,7 +32,7 @@ const HowWeWork = () => {
           <p className="mt-4 max-w-[620px] break-words font-jakarta text-lg text-[#777777] sm:mt-6">
             Our organization believes that excellent results emerge when people
             have a complete understanding of their tasks and work together while
-            developing their skills. At NExiFire, you’ll join a work environment
+            developing their skills. At NexiFire, you’ll join a work environment
             where teams work together to support your initiatives and teams work
             together to achieve their goals. Our organization creates a
             workplace environment that enables you to acquire new skills while
